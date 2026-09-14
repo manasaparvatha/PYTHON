14c8beed44a3bb14f545d4d10950f582c90934c7
manasaparvatha <manasaparvatha56@gmail.com>
2026-09-14 12:06:06 +0530

    Add files via upload

---
+++ program.py
@@ -0,0 +1,76 @@
+#display personal details using variable
+name=input("enter your name")
+age=int(input("enter your age"))
+height=float(input("enter your number"))
+print(name)
+print(age)
+print(height)
+
+
+#personalized greeting 
+name=input("enter your name")
+print(F"Hello,{name}!")
+
+
+#add two numbers read as string
+a=input("enter your numder:")
+b=input("enter your number:")
+a=int(a)
+b=int(b)
+total=a+b
+print("total:",total)
+
+#float to integer conversion
+n=float(input("enter number:"))
+print(n)
+new=int(n)
+print(new)
+
+#sum using arthemetic operator
+a=int(input("enter number:"))
+b=int(input("enter number:"))
+print(a+b)
+
+length=float(input("enter length:"))
+breadth=float(input("enter breadth:"))
+area=length*breadth
+print(area)
+
+
+#quotient and remainder
+a=int(input("enter number"))
+b=int(input("enter number"))
+q=a/b
+r=a%b
+print(q)
+print(r)
+
+#power calculation
+base=int(input("enter base"))
+exponent=int(input("enter exponent"))
+total=base**exponent
+print(total)
+
+
+#grater than comparison
+a=int(input("enter number"))
+b=int(input("enter number"))
+print(a>b)
+
+#equality check
+n1=int(input("enter number"))
+n2=int(input("enter number"))
+print(n1==n2)
+
+#both number positive check
+n1=int(input("enter number"))
+n2=int(input("enter number"))
+print(n1>0 and n2>0)
+
+
+#at least one even number
+n1=int(input("enter number"))
+n2=int(input("enter number"))
+print(n1%2==0 or n2%2==0)
+
+
