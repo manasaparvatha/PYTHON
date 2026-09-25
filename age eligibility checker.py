539d934bd969643c698dbfa3926b41a96216e796
manasaparvatha <manasaparvatha56@gmail.com>
2026-09-25 07:40:06 +0530

    Add files via upload

---
+++ age eligibility checker.py
@@ -0,0 +1,48 @@
+#identity operators
+a=None
+print(a is None)
+print(a is not None)
+
+#bitwise Operators 
+a=5
+b=3
+print(a&b)
+print(a|b)
+print(a^b)
+print(a<<b)
+print(a>>b)
+
+a=8
+b=2
+print(a<<b)
+a=13
+b=5
+print(a>>b)
+print(a<<b)
+
+a=7
+b=4
+print(7<<4)
+
+#electricity bill caluclate
+units =int(input("enter electricity units:"))
+rate=6
+bill=units*rate
+print("electricity Bill:",bill)
+
+#travel expense caluclator
+travel=float(input("travel expense:"))
+food=float(input("food expense:"))
+hotel=float(input("hotel expense:"))
+total=travel+food+hotel
+print("total expense:",total)
+
+#list in python
+marks=[80,90,95,75]
+print(marks)
+
+#accessing elements in a list
+marks=[80.90,95,75]
+print(marks[0])
+print(marks[2])
+
