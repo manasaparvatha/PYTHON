9e92e1f80b5308fd445534168f2740e68dc740a2
manasaparvatha <manasaparvatha56@gmail.com>
2026-09-25 07:41:09 +0530

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,16 @@
+#sets in python
+#sets is a collection of unique values and it ia unordable and mutable
+numbers={10,20,30,20,10}
+print(numbers)
+
+
+#suppose students have selected subjects
+subjects={"python","java","python","sol","java"}
+print(subjects)
+
+#add values to a set
+subjects={"python","sol"}
+subjects.add("java")
+print(subjects)C:\Users\lokes\OneDrive\Desktop\web developement\cseaiml.html
+
+
